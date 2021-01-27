ecf502f01629e91601c541639ae30e4b11524a75
AIvanov <isanioka@gmail.com>
2021-01-27 22:56:12 +0300

    ChatListItem

---
+++ components/ChatListItem/style.ts
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  leftContainer: {
+    flexDirection: 'row',
+  },
+  midContainer: {
+    justifyContent: 'space-around',
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 15,
+  },
+  username: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  lastMessage: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  time: {
+    fontSize: 16,
+    color: 'gray',
+  }
+})
+
+export default style;
--- navigation/MainTabNavigator.tsx
+++ navigation/MainTabNavigator.tsx
@@ -6,7 +6,7 @@ import * as React from 'react';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
-import TabOneScreen from '../screens/TabOneScreen';
+import ChatsScreen from '../screens/ChatsScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
 import { MainTabParamList, TabOneParamList, TabTwoParamList } from '../types';
 
@@ -45,7 +45,7 @@ export default function MainTabNavigator() {
       />
       <MainTab.Screen
         name="Chats"
-        component={TabTwoNavigator}
+        component={ChatsScreen}
       />
       <MainTab.Screen
         name="Status"
@@ -74,7 +74,7 @@ function TabOneNavigator() {
     <TabOneStack.Navigator>
       <TabOneStack.Screen
         name="TabOneScreen"
-        component={TabOneScreen}
+        component={ChatsScreen}
         options={{ headerTitle: 'Tab One Title' }}
       />
     </TabOneStack.Navigator>
